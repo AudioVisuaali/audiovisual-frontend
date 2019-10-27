f41ae9c6db018116f04e53c7a471dce071409b5a
Audiovisuaali <aleksi.vuorio@yahoo.com>
2019-10-27 03:09:29 +0300

    update message display style

--- app/containers/Chat/MessageVideoSkip.js
+++ app/containers/Chat/MessageVideoSkip.js
@@ -4,14 +4,17 @@ import A from 'components/A';
 import Wrapper from './styles/Wrapper';
 import Bolded from './styles/Bolded';
 
-const MessageVideoSkip = ({ message }) => (
-  <Wrapper>
-    skipped video{' '}
-    <A href={message.content.url}>
-      <Bolded>{message.content.title}</Bolded>
-    </A>
-  </Wrapper>
-);
+const MessageVideoSkip = ({ message }) =>
+  message.content ? (
+    <Wrapper>
+      skipped, now playing{' '}
+      <A href={message.content.url}>
+        <Bolded>{message.content.title}</Bolded>
+      </A>
+    </Wrapper>
+  ) : (
+    <Wrapper>skipped</Wrapper>
+  );
 
 MessageVideoSkip.propTypes = {
   message: PropTypes.shape({
--- app/containers/Chat/styles/Wrapper.js
+++ app/containers/Chat/styles/Wrapper.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   flex-grow: 1;
+  color: rgba(255, 255, 255, 0.8);
 `;
 
 export default Wrapper;
