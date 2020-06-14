7dff9e544fb47ee2ba6b746e6edbcb646c0d567a
audiovisuaali <aleksi.vuorio@yahoo.com>
2020-06-14 11:13:12 +0300

    Fix crash on homepage

--- app/containers/HomePage/styles/Container.js
+++ app/containers/HomePage/styles/Container.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 import fadeAndSlideInFromTop from 'styles/animations/fadeAndSlideInFromTop';
 
-export default styled.form({
-  flexShrink: 0,
-  maxWidth: 420,
-  width: '100%',
-  margin: '10px 10px 30px',
+export default styled.form`
+  flex-shrink: 0;
+  max-width: 420px;
+  width: 100%;
+  margin: 10px 10px 30px;
 
-  animation: `${fadeAndSlideInFromTop} ease 1s`,
-});
+  animation: ${fadeAndSlideInFromTop} ease 1s;
+`;
